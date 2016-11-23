8cd76e2ab2a8a447c0a68f6d0117d08cba296d58
Tobi Décary-Larocque <tobdl09@hotmail.com>
2016-11-23 11:36:04 -0500

    slight tweaks
    Slight changes to get better results

--- script.py
+++ script.py
@@ -3,22 +3,25 @@ import webbrowser, BeautifulSoup ,requests
 print('Enter your programming related question:')
 question = raw_input()
 
-res = requests.get('https://www.google.ca/search?q=' + question+" stack+overflow")
+res = requests.get('https://www.google.ca/search?q=' + question+' site:stackoverflow.com')
 
 res.raise_for_status()
 
-soup = BeautifulSoup.BeautifulSoup(res.text)
+soup = BeautifulSoup.BeautifulSoup(res.text, "html.parser")
 
 text = str(soup).split()
 
 link = ""
 
 for x in text:
-    if "stackoverflow" in x:
-        link = x[13:].split("\"")[0]
+    if "http://stackoverflow.com/questions" in x and not 'webcache' in x:
+        link = x[13:].split("&")[0]
+        print(link)
         break
+
 
 if link != "":
     webbrowser.open(link)
+    pass
 else:
     print("Sorry, but no answer was found for your question")
